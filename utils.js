5370109fa55f098543fdd3718e57c6d2d0fb4bdf
akn714 <adarshkumar20012704@gmail.com>
2025-05-09 16:36:20 +0530

    updated authorizeUser in utils.js, and fixed formating

--- utils.js
+++ utils.js
@@ -1,32 +1,42 @@
+const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'your_super_secret_key'; // move to .env in real projects
+const Users = require('../models/Users.model');
+
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET; // move to .env in real projects
 
 module.exports.errorHandler = function (err, req, res, next) {
-    console.error('[-]', err.stack);
-    res.status(500).json({ message: 'Something broke!' });
+  console.error('[-]', err.stack);
+  res.status(500).json({ message: 'Something broke!' });
 }
 
 module.exports.log = (req, res, next) => {
-    console.log('[+]', req.method, req.url);
-    next();
+  console.log('[+]', req.method, req.url);
+  next();
 }
 
+module.exports.authorizeUser = async (req, res, next) => {
+  console.log('[+] authorizing user');
+  // const id = is_user_authentic(req.cookies.token);
 
-module.exports.authorizeUser = (req, res, next) => {
-    console.log('[+] authorizing user');
-    const authHeader = req.headers.authorization;
+  // const authHeader = req.headers.authorization;
+  // if (!authHeader || !authHeader.startsWith('Bearer ')) {
+  //   return res.status(401).json({ error: 'Unauthorized: No token provided' });
+  // }
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ error: 'Unauthorized: No token provided' });
-    }
+  // const token = authHeader.split(' ')[1];
 
-    const token = authHeader.split(' ')[1];
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+    const id = payload.id;
+    // if (!id) return res.status(404).json({ message: 'Invalid Token!' });
+    
+    const user = await Users.findById(id);
+    if(!user) return res.status(404).json({ message: 'User not found' });
 
-    try {
-        const decoded = jwt.verify(token, JWT_SECRET);
-        req.id = decoded.id; // assuming you encoded { id: user._id }
-        next();
-    } catch (err) {
-        return res.status(401).json({ error: 'Unauthorized: Invalid token' });
-    }
+    req.id = payload.id;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Unauthorized: Invalid token' });
+  }
 };
